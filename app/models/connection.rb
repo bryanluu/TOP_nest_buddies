c3ad8a3ab883ef3c5c1b5b53d9c15b39049a6b21
Bryan Luu <bluu@proofgov.com>
2021-10-26 19:41:17 -0700

    :children_crossing: make Connections reflexive

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,4 +1,12 @@
 class Connection < ApplicationRecord
   belongs_to :user
   belongs_to :buddy, class_name: "User"
+
+  after_create :create_inverse
+
+  def create_inverse
+    self.class.create(user_id: buddy.id, buddy_id: user.id)
+  end
+
+  validates_uniqueness_of :user_id, scope: :buddy_id
 end

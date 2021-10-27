3b1d92f6ce95b61ee8d6b92df6a9777856ee22a1
Bryan Luu <bluu@proofgov.com>
2021-10-26 19:36:02 -0700

    :building_construction: add basic Connections associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :connections, dependent: :destroy
+  has_many :buddies, through: :connections
 end

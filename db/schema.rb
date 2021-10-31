f05a525bde7c92c627a27e17d89f26c6ae5e302b
Bryan Luu <loc.bryan.luu@gmail.com>
2021-10-31 16:27:40 -0700

    :card_file_box: create Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_024543) do
+ActiveRecord::Schema.define(version: 2021_10_31_232503) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,12 @@ ActiveRecord::Schema.define(version: 2021_10_27_024543) do
     t.boolean "accepted", default: false
     t.index ["buddy_id"], name: "index_connections_on_buddy_id"
     t.index ["user_id"], name: "index_connections_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|

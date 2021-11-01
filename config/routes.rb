4f1739808e2d355e98eb16a5ccf1ec4356fb5f71
Bryan Luu <bluu@proofgov.com>
2021-10-31 22:10:11 -0700

    :sparkles: add url/path helpers for requests

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
 
-  post 'connection/:buddy_id', to: 'connection#create'
-  put 'connection/:buddy_id', to: 'connection#accept'
-  delete 'connection/:buddy_id', to: 'connection#decline'
+  post 'connection/:buddy_id', to: 'connection#create', as: 'request_connection'
+  put 'connection/:buddy_id', to: 'connection#accept', as: 'accept_request'
+  delete 'connection/:buddy_id', to: 'connection#decline', as: 'decline_request'
 
   root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

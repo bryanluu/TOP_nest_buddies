b302442320fe13bff58acd889713d84a58050c74
Bryan Luu <loc.bryan.luu@gmail.com>
2021-11-03 19:42:02 -0700

    :butterfly: rename text column to content

---
+++ db/migrate/20211104023955_rename_text_column_to_content_for_posts.rb
@@ -0,0 +1,5 @@
+class RenameTextColumnToContentForPosts < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :posts, :text, :content
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_04_023333) do
+ActiveRecord::Schema.define(version: 2021_11_04_023955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2021_11_04_023333) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.text "text", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "author_id"

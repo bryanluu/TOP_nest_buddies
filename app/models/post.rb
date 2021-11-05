2249532219cc629c89a059d92030d735b408b3f7
Bryan Luu <loc.bryan.luu@gmail.com>
2021-11-04 19:44:45 -0700

    :sparkles: add association between Comments, Users, Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :likes
   has_many :fans, through: :likes, source: :user
+  has_many :comments
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_05_022056) do
+ActiveRecord::Schema.define(version: 2021_11_05_023606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_11_05_022056) do
     t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_comments_on_author_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
@@ -62,6 +64,7 @@ ActiveRecord::Schema.define(version: 2021_11_05_022056) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "connections", "users"
   add_foreign_key "connections", "users", column: "buddy_id"
   add_foreign_key "likes", "posts"
